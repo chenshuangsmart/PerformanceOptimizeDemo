ce83349102630626007a23f5e7f2228acb76e081
chenshuang <1091277906@qq.com>
2019-04-06 21:20:17 +0800

    高效获取文件属性
    高效获取文件属性

--- PerformanceOptimizeDemo/PerformanceOptimizeDemo/ViewController.m
+++ PerformanceOptimizeDemo/PerformanceOptimizeDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <time.h>
+#import <sys/stat.h>
 
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
@@ -18,8 +20,8 @@
 @property(nonatomic,strong)UIImageView *imgView2;
 /** datestr*/
 @property(nonatomic,strong)NSString *dateStr;
-/** timeLbe*/
-@property(nonatomic,strong)UILabel *timeLbe;
+/** date format*/
+@property(nonatomic,strong)NSDateFormatter *formatter;
 @end
 
 @implementation ViewController
@@ -31,8 +33,16 @@
 //    [self drawImgView];
     
     // NSDateFormatter
-    [self convertDateToStringUsingDateFormatter];
-    [self convertDateToStringUsingCLocaltime];
+//    [self convertDateToStringUsingDateFormatter];
+//    [self convertDateToStringUsingCLocaltime];
+    
+    // dateStr -> date
+//    [self compareTime];
+//    [self compareTime2];
+//    [self getTime];
+    
+    [self getFileAttrByFileManager];
+    [self getFileAttrByStat];
 }
 
 #pragma mark - imageNamed 与 imageWithContentsOfFile对比
@@ -78,6 +88,86 @@ static double old, now;
     NSLog(@"convertDateToStringUsingDateFormatter: %f",now - old);
 }
 
+#pragma mark - dateFromString
+
+#define kTimeInterval0 @"100000"
+
+// 2019-04-06 20:11:13
+#define kTimeIntervalStr @"2019-04-06 20:11:13"
+#define kTimeInterval @"1554552673"
+#define kTimeInterval2 @"1554552999"
+
+- (void)compareTime {
+    old = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < TIMES; i++) {
+        NSDate* startDate = [self dateFromString:@"1554552673"];
+        NSDate* endDate = [self dateFromString:@"1554552999"];
+        NSTimeInterval time = [endDate timeIntervalSinceDate:startDate];
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"dateFromString: %f",now - old);
+}
+
+- (void)compareTime2 {
+    old = CFAbsoluteTimeGetCurrent();
+    for (int i = 0; i < TIMES; i++) {
+        NSDate* startDate = [self strptimeFromString:@"2019-04-06 20:11:13"];
+        NSDate* endDate = [self strptimeFromString:@"2019-04-06 20:46:13"];
+        NSTimeInterval time = [endDate timeIntervalSinceDate:startDate];
+    }
+    now = CFAbsoluteTimeGetCurrent();
+    NSLog(@"dateWithTimeIntervalSince1970: %f",now - old);
+}
+
+- (void)getTime {
+    NSDate *date = [self strptimeFromString:kTimeIntervalStr];
+    NSString *time = [self.formatter stringFromDate:date];
+    NSLog(@"time = %@",time);
+}
+
+- (NSDate *)dateFromString:(NSString *)timeInterval {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"]];
+    [formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSDate *date = [formatter dateFromString:timeInterval];
+    return date;
+}
+
+- (NSDate *)strptimeFromString:(NSString *)timeInterval {
+    time_t t;
+    struct tm tm;
+    strptime([timeInterval cStringUsingEncoding:NSUTF8StringEncoding], "%Y-%m-%dT%H:%M:%S%z", &tm);
+    tm.tm_isdst = -1;
+    t = mktime(&tm);
+    NSDate *date = [NSDate dateWithTimeIntervalSince1970:t + [[NSTimeZone localTimeZone] secondsFromGMT]];
+    return date;
+}
+
+#pragma mark - get file attr
+
+- (void)getFileAttrByFileManager {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"address" ofType:@"plist"];
+    NSError *error;
+    NSDictionary *attrDict = [[NSFileManager defaultManager] attributesOfItemAtPath:path error:&error];
+    if (error) {
+        NSLog(@"error = %@",error.description);
+    } else {
+        NSLog(@"attrDict = %@",attrDict);
+    }
+}
+
+- (void)getFileAttrByStat {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"address" ofType:@"plist"];
+    struct stat statbuf;
+    const char *cpath = [filePath fileSystemRepresentation];
+    if (cpath && stat(cpath, &statbuf) == 0) {
+        NSNumber *fileSize = [NSNumber numberWithUnsignedLongLong:statbuf.st_size];
+        NSDate *creationDate = [NSDate dateWithTimeIntervalSince1970:statbuf.st_ctime];
+        NSDate *modificationDate = [NSDate dateWithTimeIntervalSince1970:statbuf.st_mtime];
+        NSLog(@"fileSize = %ld, creationDate = %@, modificationDate = %@",(long)[fileSize integerValue],[self.formatter stringFromDate:creationDate],[self.formatter stringFromDate:modificationDate]);
+    }
+}
+
 #pragma mark - lazy
 
 - (UIImageView *)imgView1 {
@@ -96,15 +186,13 @@ static double old, now;
     return _imgView2;
 }
 
-- (UILabel *)timeLbe {
-    if (_timeLbe == nil) {
-        _timeLbe = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 400, 40)];
-        _timeLbe.textColor = [UIColor blackColor];
-        _timeLbe.textAlignment = NSTextAlignmentCenter;
-        _timeLbe.center = self.view.center;
-        [self.view addSubview:_timeLbe];
+- (NSDateFormatter *)formatter {
+    if (_formatter == nil) {
+        _formatter = [[NSDateFormatter alloc] init];
+        [_formatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"zh_CN"]];
+        [_formatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
     }
-    return _timeLbe;
+    return _formatter;
 }
 
 @end
